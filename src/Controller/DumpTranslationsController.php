478a616afb00a6bb85f73112e887b98d13e995e0
Jannik Zschiesche <jz@becklyn.com>
2019-07-25 21:26:44 +0200

    Smaller issues fixed
    + Fixed controller name
    + Added requirements for locale
    + use native Symfony JsonResponse

--- src/Controller/DumpTranslationsController.php
+++ src/Controller/DumpTranslationsController.php
@@ -5,10 +5,11 @@ namespace Becklyn\Translations\Controller;
 use Becklyn\Translations\Extractor\TranslationsExtractor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DumpTranslationsCommand extends AbstractController
+class DumpTranslationsController extends AbstractController
 {
     /**
      * @param string $locale
@@ -18,18 +19,23 @@ class DumpTranslationsCommand extends AbstractController
         ParameterBagInterface $parameters,
         Request $request,
         string $locale
-    )
+    ) : Response
     {
         $isDebug = $parameters->get("kernel.debug");
         $catalogue = $extractor->fetchCatalogue($locale, !$isDebug);
 
-        $response = new Response(
-            "window.TranslatorInit.init(JSON.parse('{$catalogue->getCatalogueJson()}'));",
+        $response = new JsonResponse(
+            // use JSON parse and a string here, as it is way faster than parsing JavaScript in the browser.
+            "JSON.parse('{$catalogue->getCatalogueJson()}')",
             200,
             [
-                "Content-Type" => "application/javascript; charset=utf-8",
-            ]
+                // prevent magic byte insertion
+                "X-Content-Type-Options" => "nosniff",
+            ],
+            true
         );
+
+        $response->setCallback("window.TranslatorInit.init");
 
         if (!$isDebug)
         {
